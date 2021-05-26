12df9d19ace220946d00fad0d22ca877532b74d7
KRISHNA KUAMR <krk5512@gmail.com>
2021-05-26 20:17:19 +0530

    clibasedNotes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,11 @@
-// Challenge 1
-// const add = require('./utils.js')
-// const sum = add(5,5);
-// console.log(sum);
-
-// Challenge 2
-// const notesFunc = require('./notes');
-// const validator = require('validator');
-// console.log(validator.isEmail('krishna@gmail.com'));
-// console.log(validator.isURL('https://www.google.com'))
-// console.log(notesFunc())
-
-// Challenge 3
-// installed nodemon to excecute node js file automatically
-
-// const chalk = require('chalk');
-// console.log(chalk.green('Success'))
-// console.log(chalk.bold.red('TRY AGAIN'))
-// console.log(chalk.bold.blue.inverse('Success'))
+const getNotes = require('./notes');
+const chalk = require('chalk');
+const fs = require('fs');
+console.log(getNotes())
+const command = process.argv[2];
+console.log(process.argv)
+if (command === 'add') {
+    console.log('Adding Note')
+} else if (command === 'remove') {
+    console.log('Remove note')
+}
---
+++ notes-app/challengesCode.js
@@ -0,0 +1,20 @@
+// Challenge 1
+// const add = require('./utils.js')
+// const sum = add(5,5);
+// console.log(sum);
+
+// Challenge 2
+// const notesFunc = require('./notes');
+// const validator = require('validator');
+// console.log(validator.isEmail('krishna@gmail.com'));
+// console.log(validator.isURL('https://www.google.com'))
+// console.log(notesFunc())
+
+// Challenge 3
+// installed nodemon to excecute node js file automatically
+// const chalk = require('chalk');
+// console.log(chalk.green('Success'))
+// console.log(chalk.bold.red('TRY AGAIN'))
+// console.log(chalk.bold.blue.inverse('Success'))
+
+
