6c497021d7f34aa0ac516e98ed696bc4342a8d03
KRISHNA KUAMR <krk5512@gmail.com>
2021-05-26 16:03:09 +0530

    nodemon installation

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,8 +4,16 @@
 // console.log(sum);
 
 // Challenge 2
-const notesFunc = require('./notes');
-const validator = require('validator');
-console.log(validator.isEmail('krishna@gmail.com'));
-console.log(validator.isURL('https://www.google.com'))
-console.log(notesFunc())
+// const notesFunc = require('./notes');
+// const validator = require('validator');
+// console.log(validator.isEmail('krishna@gmail.com'));
+// console.log(validator.isURL('https://www.google.com'))
+// console.log(notesFunc())
+
+// Challenge 3
+// installed nodemon to excecute node js file automatically
+
+// const chalk = require('chalk');
+// console.log(chalk.green('Success'))
+// console.log(chalk.bold.red('TRY AGAIN'))
+// console.log(chalk.bold.blue.inverse('Success'))
