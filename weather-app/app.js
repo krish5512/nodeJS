0ea9a54d47b1976ec6ea74ea05551103f5af2b44
KRISHNA KUAMR <krk5512@gmail.com>
2021-05-31 21:16:45 +0530

    weatherAPI -Data parsing

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,12 @@
 const request = require('request');
 
 
-const url = 'http://api.weatherstack.com/current?access_key=9c107dc6a7a681679e8429f819497b6d&query=28.643734,77.345399';
+const url = 'http://api.weatherstack.com/current?access_key=9c107dc6a7a681679e8429f819497b6d&query=28.643734,77.345399&units=f';
 
 request({
     url: url,
     json: true
 }, (error, response) => {
-    console.log(response.body.current)
+    const current = response.body.current;
+    console.log(current.weather_descriptions[0] + " It is currently " + current.temperature + " degrees out, It feels like " + current.feelslike + " degrees out.")
 })
