b5dcf7d86fe3c9edbfc7b1b968d273ed2a10105e
KRISHNA KUAMR <krk5512@gmail.com>
2021-06-02 20:50:47 +0530

    user based Input for weather app by CLI

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -20,3 +20,22 @@ const forecast = (address, callback) => {
 }
 
 module.exports = forecast;
+
+
+
+// const weatherUrl = 'http://api.weatherstack.com/current?access_key=YOUR_API_KEY&query=28.643734,77.345399&units=f';
+
+// request({
+//     url: weatherUrl,
+//     json: true
+// }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect Weather Service')
+//     } else if (response.body.error) {
+//         console.log('Unable to find the location')
+//     } else {
+//         const current = response.body.current;
+//         console.log(current.weather_descriptions[0] + " It is currently " + current.temperature + " degrees out, It feels like " + current.feelslike + " degrees out.")
+//     }
+
+// })
--- weather-app/utils/geoCode.js
+++ weather-app/utils/geoCode.js
@@ -18,4 +18,27 @@ const geoCode = (address, callBack) => {
 }
 
 module.exports = geoCode;
-// 
+
+
+
+
+// // Geocoding Service
+
+// // Address -> Lat/Long -> Weather
+// const loactionUrl = 'http://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&limit=1&access_token=YOUR_API_KEY';
+// request({
+//     url: loactionUrl,
+//     json: true
+// }, (
+//     error, resp
+// ) => {
+//     if (error) {
+//         console.log('Unable to connect Location Service')
+//     } else if (resp.body.features.length === 0) {
+//         console.log('Invalid request data')
+//     } else {
+//         const latitude = resp.body.features[0].center[1];
+//         const longitude = resp.body.features[0].center[0];
+//         console.log('THe location is ' + latitude + " and " + longitude)
+//     }
+// })
