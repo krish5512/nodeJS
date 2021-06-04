dd60c645782f0b091821635a46635874bd06d13f
KRISHNA KUAMR <krk5512@gmail.com>
2021-06-04 21:54:24 +0530

    expressJS

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,32 @@
 const express = require('express')
 
+console.log(__dirname);
+console.log(__filename)
 const app = express();
 
+
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('<h1>Weather</h1>');
 })
 
 
-// app.com
-// app.com/help
-// app.com/about
+app.get('/help', (req, res) => {
+
+    res.send({
+        name: 'Krishna',
+        age: 21
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
+})
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast : 'Its 34 degree celcius and with scatter rain',
+        location : 'vaishali'
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
