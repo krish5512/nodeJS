3bd0e685298eecd04a7ebd04a275d4a0d4d2bfdb
KRISHNA KUAMR <krk5512@gmail.com>
2021-06-05 16:35:37 +0530

    handleBar Configured

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,33 +1,37 @@
+const path = require('path')
 const express = require('express')
-
-console.log(__dirname);
-console.log(__filename)
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.set('view engine', 'hbs');
 
+app.use(express.static(publicDirectoryPath))
 
-app.get('/', (req, res) => {
-    res.send('<h1>Weather</h1>');
-})
-
-
-app.get('/help', (req, res) => {
-
-    res.send({
-        name: 'Krishna',
-        age: 21
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App'
     })
 })
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
 app.get('/weather', (req, res) => {
     res.send({
-        forecast : 'Its 34 degree celcius and with scatter rain',
-        location : 'vaishali'
+        forecast: 'Its 34 degree celcius and with scatter rain',
+        location: 'vaishali'
     })
 })
-
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+    console.log('Server is up on port 3000.');
 })
+
+
+
+
+// Express page routing Example
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'Krishna',
+//         age: 21
+//     })
+// })
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+// })
