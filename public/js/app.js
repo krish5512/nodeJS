edb75c179573351c28916c557ad09c6ce866e8e0
KRISHNA KUAMR <krk5512@gmail.com>
2021-06-08 21:56:18 +0530

    webServerConfihgured and added

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-console.log('Client side JS file')
+console.log('Client side JS file');
+fetch('http://localhost:3000/weather?address=vaishali').then((response) => {
+        response.json().then((data) => {
+            if(data.error)
+            {
+                console.log(error)
+            }
+            else{
+                console.log(data)
+            }
+        })  
+});
---
+++ src/utils/geoCode.js
@@ -0,0 +1,47 @@
+const request = require('request')
+const geoCode = (address, callBack) => {
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?proximity=-74.70850,40.78375&limit=1&access_token=pk.eyJ1Ijoia3Jpc2huYTMyMSIsImEiOiJja3BkbWRqcWYwMHhjMndxcnkwZmRtMGRjIn0.cDuAik58B0F-2_ZgUiOKgQ'
+
+    request({
+        url,
+        json: true
+    }, (error, response) => {
+        if (error) {
+            callBack('Unable to connect Location Service', undefined)
+        } else if (response.body.features.length === 0) {
+            callBack('Unable to find location', undefined)
+        } else {
+            callBack(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+            })
+        }
+    })
+
+}
+
+module.exports = geoCode;
+
+
+
+
+// // Geocoding Service
+
+// // Address -> Lat/Long -> Weather
+// const loactionUrl = 'http://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&limit=1&access_token=YOUR_API_KEY';
+// request({
+//     url: loactionUrl,
+//     json: true
+// }, (
+//     error, resp
+// ) => {
+//     if (error) {
+//         console.log('Unable to connect Location Service')
+//     } else if (resp.body.features.length === 0) {
+//         console.log('Invalid request data')
+//     } else {
+//         const latitude = resp.body.features[0].center[1];
+//         const longitude = resp.body.features[0].center[0];
+//         console.log('THe location is ' + latitude + " and " + longitude)
+//     }
+// })
