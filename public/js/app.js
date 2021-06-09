ebe0cb6293bde65224b68aa1750def6b0e5df10c
KRISHNA KUAMR <krk5512@gmail.com>
2021-06-09 17:38:23 +0530

    weather -web-server completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,30 @@
 console.log('Client side JS file');
-fetch('http://localhost:3000/weather?address=vaishali').then((response) => {
-        response.json().then((data) => {
-            if(data.error)
-            {
-                console.log(error)
-            }
-            else{
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value;
+    messageOne.textContent = 'Loading ....';
+    messageTwo.textContent = '';
+    if (location !== '') {
+        const url = 'http://localhost:3000/weather?address=' + location;
+        fetch(url).then((response) => {
+            response.json().then((data) => {
                 console.log(data)
-            }
-        })  
-});
+                if (data.error) {
+                    messageOne.textContent = '';
+                    messageTwo.textContent = data.error;
+                } else {
+                    console.log(data)
+                    messageOne.textContent = 'The current for temperature is ' + data.temperature + ' with ' + data.desc[0] + ' at ' + data.address;
+                    messageTwo.textContent = '';
+                }
+            })
+        })
+    } else {
+        alert('No location entered')
+    }
+
+})
